6cf228dc1aa8396a612a27d817d0b6fea9ae5503
Allen K Chiang <akchiang@ucsd.edu>
2019-04-29 13:02:26 -0700

    Added code from petsapp for firebase backend test, added addItem router and hbs file.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sassMiddleware = require('node-sass-middleware');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
+const addItemRouter = require('./routes/addItem');
+
 
 const app = express();
 
@@ -30,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/addItem', addItemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
