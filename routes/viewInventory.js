14ce623555f5e7d34ed234df6ce807f081298285
alchiang <akchiang@ucsd.edu>
2019-05-08 15:27:19 -0700

    Trying to figure out dynamic pages...

--- routes/viewInventory.js
+++ routes/viewInventory.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const firebase = require('firebase');
+
+var config = {
+  apiKey: "AIzaSyB6-MM2GPenEPXa8F4B2j-tIrMH9J11LAc",
+  authDomain: "cogs121-app.firebaseapp.com",
+  databaseURL: "https://cogs121-app.firebaseio.com",
+  projectId: "cogs121-app",
+  storageBucket: "cogs121-app.appspot.com",
+  messagingSenderId: "115116152043"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('viewInventory', {title: "View Inventory"});
 });
 
+router.get('/inventoryItem', (req, res) => {
+  if (req) {
+    console.log(req);
+  }
+});
 module.exports = router;
