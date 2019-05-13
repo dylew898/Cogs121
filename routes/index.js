d5d5c021a5e13855ba8ddacab16702a18de3a408
Allen K Chiang <akchiang@ucsd.edu>
2019-05-13 00:05:11 -0700

    Added 'Add Items' option view

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('viewInventory', { 
-    title: 'Inventory App',
-    style: 'index.css'
+    title: 'Inventory App'
   });
 });
 
